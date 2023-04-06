942f53df891a0ca0a9aa11c378d27b9206007a8d
Marius Gripp <marius.gripp@arcor.de>
2023-04-06 16:13:36 +0200

    wip

--- Pokemon Unity/Assets/Scripts/Services/MobileInput.cs
+++ Pokemon Unity/Assets/Scripts/Services/MobileInput.cs
@@ -28,4 +28,65 @@ public class MobileInput : MonoBehaviour
         yield return new WaitForEndOfFrame();
         pressed[name] = false;
     }
+
+    bool right = false;
+    bool left = false;
+    bool down = false;
+    bool up = false;
+
+    private void Update()
+    {
+        bool rightBefore = right;
+        right = Input.GetAxis("Horizontal") > .9f;
+        if (right)
+        {
+            if (right != rightBefore)
+                Press("Right");
+        } else if (!pressed["Right"])
+        {
+            Release("Right");
+        }
+
+        bool leftBefore = left;
+        left = Input.GetAxis("Horizontal") < -.9f;
+        if (left)
+        {
+            if (left != leftBefore)
+                Press("Left");
+        }
+        else if (!pressed["Left"])
+        {
+            Release("Left");
+        }
+
+        bool downBefore = down;
+        down = Input.GetAxis("Vertical") < -.9f;
+        if (down)
+        {
+            if (down != downBefore)
+                Press("Down");
+        }
+        else if (!pressed["Down"])
+        {
+            Release("Down");
+        }
+
+        bool upBefore = up;
+        up = Input.GetAxis("Vertical") > .9f;
+        if (up)
+        {
+            if (up != upBefore)
+                Press("Up");
+        }
+        else if (!pressed["Up"])
+        {
+            Release("Up");
+        }
+
+
+        //(inputName == "Right" &&  ||
+        //        (inputName == "Left" && Input.GetAxis("Horizontal") == -1f) ||
+        //        (inputName == "Up" && Input.GetAxis("Vertical") == 1f) ||
+        //        (inputName == "Down" && Input.GetAxis("Vertical") == -1f
+    }
 }
